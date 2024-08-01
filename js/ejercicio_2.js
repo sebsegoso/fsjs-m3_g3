b56ba010c6996c2d4947fe99f1f176c6b5713854
V.Tardón <148594978+Envyllawliet@users.noreply.github.com>
2024-07-31 20:13:15 -0400

    Update ejercicio_2.js
    se agrega funcion ejercicio 2

--- js/ejercicio_2.js
+++ js/ejercicio_2.js
@@ -1,2 +1,14 @@
 /* 2. Construya una función que imprima si un número es primo o no, los números
  primos son aquellos que son divisibles solo por 1 y por sí mismos. */
+
+function primo(n) {
+
+    for (let i= 2; i < n; i++) {
+        if (n % i === 0) {
+            return false
+        }
+    }
+    return n !== 1;
+}
+
+console.log(primo(13))
